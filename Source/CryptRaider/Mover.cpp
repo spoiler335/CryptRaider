45373cec84b89673c48aea52b02e37a3841fcadb
Spoiler335 <sushantsai8@gmail.com>
2025-08-18 23:30:44 -0400

    Add mover Actor Component

---
+++ Source/CryptRaider/Mover.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Mover.h"
+#include "Math/UnrealMathUtility.h"
+
+// Sets default values for this component's properties
+UMover::UMover()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+// Called when the game starts
+void UMover::BeginPlay()
+{
+	Super::BeginPlay();
+
+	orignalLocation = GetOwner()->GetActorLocation();
+}
+
+
+// Called every frame
+void UMover::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (shouldMove)
+	{
+		FVector currLocation = GetOwner()->GetActorLocation();
+		FVector targetLoc = orignalLocation + moveOffset;
+		float speed = FVector::Distance(orignalLocation, targetLoc) / moveTime;
+
+		FVector newLocation = FMath::VInterpConstantTo(currLocation, targetLoc, DeltaTime, speed);
+
+		GetOwner()->SetActorLocation(newLocation);
+	}
+}
